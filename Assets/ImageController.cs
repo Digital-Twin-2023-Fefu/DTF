6fc6347188bb426e5d4e06dd4988fe1dcf74671d
12 <opter12345@gmail.com>
2023-10-21 17:28:33 +1000

    add min work proj

---
+++ Assets/ImageController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ImageController : MonoBehaviour
+{
+    // ячейка под отслеживаемый объект
+    public GameObject Anchor;
+
+    // ячейка под объект, который устанавливается относительно Anchor
+    public GameObject Room;
+    
+    public bool SetAnchor;
+
+    // скрипт, отвечающий за отслеживание изображений (меток)
+    private ARTrackedImageManager ARTrackedImageManagerScript;
+
+    private void Awake()
+    {
+        // Создание ссылки на скрипт, , отвечающий за отслеживание изображений (меток) 
+        ARTrackedImageManagerScript = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Нахождение в сцене объекта с тэгом Anchor
+        Anchor = GameObject.FindWithTag("Anchor");
+        // Присваивание объекту имени Anchor
+        Anchor.name = "Anchor";
+
+        if (SetAnchor)
+        {
+            // Установка объекта Room в место объекта Anchor
+            Instantiate(Room, Anchor.transform.position, Anchor.transform.rotation);
+
+            // Отключение отслеживания изображений (меток)
+            ARTrackedImageManagerScript.SetTrackablesActive(false);
+
+            SetAnchor = false;
+        }
+
+    }
+}
---
+++ Assets/SetRoom.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetRoom : MonoBehaviour
+{
+    private Button Button;
+
+    private ImageController ImageControllerScript;
+
+    void Start()
+    {
+        ImageControllerScript = FindObjectOfType<ImageController>();
+
+        Button = GetComponent<Button>();
+        Button.onClick.AddListener(AnchorEnables);
+    }
+
+    // Update is called once per frame
+    private void AnchorEnables()
+    {
+        ImageControllerScript.SetAnchor = true;
+        Debug.Log("KJHFDSKJHFKJSDf");
+    }
+}
+
