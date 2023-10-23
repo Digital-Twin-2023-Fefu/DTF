c36fb81147c75aa9f67a009e13439c774376bca3
Евгений <gunko.2007Ev.ru.@yandex.ru>
2023-10-23 18:27:29 +1000

    track coordinates

--- Assets/CoordinateViewer.cs
+++ Assets/CoordinateViewer.cs
@@ -3,22 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 public class CoordinateViewer : MonoBehaviour
 {
     public Camera camera;
 
     public TMP_Text text;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    // static string serverIP = "10.193.162.205";
+    // static int serverPort= 8080;
+    // static TcpClient client = new TcpClient(serverIP, serverPort);
+
+
+
     void Update()
     {
         string coords = string.Format("X:{0}, Y{1}, Z{2}", camera.transform.position.x, camera.transform.position.y, camera.transform.position.z);
         text.text = coords;
+
+        string serverIP = "10.193.162.205";
+        int serverPort= 8080;
+        TcpClient client = new TcpClient(serverIP, serverPort);
+
+        NetworkStream stream = client.GetStream();
+
+        // Генерируйте новые координаты здесь
+        double coordinates1, coordinates2, coordinates3;
+        coordinates1 = camera.transform.position.x;
+        coordinates2 = camera.transform.position.y;
+        coordinates3 = camera.transform.position.z;
+        
+        string coordinates = string.Format("{0};{1};{2}", coordinates1, coordinates2, coordinates3);
+
+        byte[] data = Encoding.UTF8.GetBytes(coordinates);
+        stream.Write(data, 0, data.Length);
+
+        // byte[] data = Encoding.UTF8.GetBytes();
+        // stream.Write(data, 0, data.Length);
+        System.Console.WriteLine("Координаты отправлены: ");
     }
 }
+    
